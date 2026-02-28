e34205e0a07f1d12bc6f853435bfeb037684d3c1
Calvin Ardiansyah <calvinardiansyahpplg>
2026-02-28 19:56:06 +0700

    3.2.1 Langkah-langkah Percobaan

---
+++ Minggu3/Mahasiswa09.java
@@ -0,0 +1,13 @@
+package Minggu3;
+public class Mahasiswa09 {
+    public String nim, nama, kelas;
+    public float ipk;
+
+    void cetakInfo() {
+        System.out.println("NIM  :" + nim);
+        System.out.println("NAMA : " + nama);
+        System.out.println("KELAS:" + kelas);
+        System.out.println("IPK  :" + ipk);
+        System.out.println("---------------------------------");
+    }
+}
