6cd26ba16e6b25e53e21d18b9bc01390473743df
Calvin Ardiansyah <calvinardiansyahpplg>
2026-02-28 17:14:46 +0700

    Tambah method & modifikasi

--- Minggu 3/MatakuliahDemo.java
+++ Minggu 3/MatakuliahDemo.java
@@ -3,34 +3,21 @@ import java.util.Scanner;
 public class MatakuliahDemo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Matakuliah[] arrayOfMatakuliah = new Matakuliah[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+    
+        System.out.print("Masukkan jumlah matakuliah: ");
+        int jumlah = Integer.parseInt(input.nextLine());
+        System.out.println("-------------------");
+        Matakuliah[] arrayOfMatakuliah = new Matakuliah[jumlah];
 
         for (int i = 0; i < arrayOfMatakuliah.length; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
-            System.out.print("Kode: ");
-            kode = input.nextLine();
-            System.out.print("Nama: ");
-            nama = input.nextLine();
-            System.out.print("Sks: ");
-            dummy = input.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumalah jam: ");
-            dummy = input.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("-------------------");
-
-            arrayOfMatakuliah[i] = new Matakuliah(kode, nama, sks, jumlahJam);
+            arrayOfMatakuliah[i] = new Matakuliah();
+            arrayOfMatakuliah[i].tambahData(input);
         }
 
         for (int i = 0; i < arrayOfMatakuliah.length; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
-            System.out.println("Kode       :" + arrayOfMatakuliah[i].kode);
-            System.out.println("Nama       :" + arrayOfMatakuliah[i].nama);
-            System.out.println("Sks        :" + arrayOfMatakuliah[i].sks);
-            System.out.println("Jumlah Jam :" + arrayOfMatakuliah[i].jumlahJam);
-            System.out.println("-------------------");
+            arrayOfMatakuliah[i].cetakInfo();
         }
 
         input.close();
